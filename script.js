e3e7b29e90806cd88c4decf5c23d47d2d9cc92be
Mohammad Haris <112751385+mdharis98@users.noreply.github.com>
2024-10-27 22:05:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,31 @@
-// complete this js code
-function Person(name, age) {}
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
+}
 
-function Employee(name, age, jobTitle) {}
+Person.prototype.greet = function() {
+  console.log(`Hello, my name is ${this.name}, I am ${this.age} years old`);
+};
+
+function Employee(name, age, jobTitle) {
+  Person.call(this, name, age); // Inherit properties from Person
+  this.jobTitle = jobTitle;
+}
+
+// Inherit the Person prototype
+Employee.prototype = Object.create(Person.prototype);
+Employee.prototype.constructor = Employee;
+
+Employee.prototype.jobGreet = function() {
+  console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}`);
+};
+
+// Example Test Case
+const person = new Person("Alice", 25);
+person.greet(); // Output: Hello, my name is Alice, I am 25 years old
+
+const employee = new Employee("Bob", 30, "Manager");
+employee.jobGreet(); // Output: Hello, my name is Bob, I am 30 years old, and my job title is Manager
 
 // Do not change code below this line
 window.Person = Person;
